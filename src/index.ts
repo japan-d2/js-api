15d291c0e2f2ff2f9a1d24345a69a5ef375341f7
AOKI Yuuto <aoki@u-ne.co>
2020-03-15 02:03:57 +0900

    feat: apiFactory with endpoints

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,28 @@ import { Options, Connector, CallParameters, EndpointSchema } from './interfaces
 
 export * from './interfaces'
 
-type APICallable <O> = <T, U>(
-  endpoint: EndpointSchema<T, U>,
-  parameters: CallParameters<T, O>,
-  callOptions?: Options
-) => Promise<U>
+type EndpointMap = Record<string, EndpointSchema<{}, {}>>
 
-export function apiFactory <O extends Options> (defaultOptions: O, connector: Connector): APICallable<O> {
-  return async function api <T, U> (endpoint: EndpointSchema<T, U>, parameters: CallParameters<T, O>, callOptions?: Options): Promise<U> {
+interface APICallable <O, E extends EndpointMap = {}> {
+  <T, U>(
+    endpoint: EndpointSchema<T, U>,
+    parameters: CallParameters<T, O>,
+    callOptions?: Options
+  ): Promise<U>;
+
+  <K extends keyof E, S extends EndpointSchema<{}, {}> = E[K]>(
+    endpoint: K,
+    parameters: CallParameters<ReturnType<S['request']['getType']>, O>,
+    callOptions?: Options
+  ): Promise<ReturnType<S['response']['getType']>>;
+}
+
+export function apiFactory <O extends Options, E extends EndpointMap> (defaultOptions: O, endpoints: E, connector: Connector): APICallable<O, E> {
+  async function callWithObject <T, U> (
+    endpoint: EndpointSchema<T, U>,
+    parameters: CallParameters<T, O>,
+    callOptions?: Options
+  ): Promise<U> {
     const options = { ...defaultOptions, ...(callOptions || {}) }
     if (options.validateRequest) {
       const result = validate({
@@ -45,5 +59,25 @@ export function apiFactory <O extends Options> (defaultOptions: O, connector: Co
       }
     }
     return response as unknown as U
+  }
+
+  async function callWithId <K extends keyof E> (
+    endpoint: K,
+    parameters: CallParameters<{}, {}>,
+    callOptions?: Options
+  ): Promise<unknown> {
+    const endpointObject = endpoints[endpoint]
+    return callWithObject(endpointObject, parameters, callOptions)
+  }
+
+  return async function api (
+    endpoint: string | EndpointSchema<{}, {}>,
+    parameters: CallParameters<{}, {}>,
+    callOptions?: Options
+  ): Promise<unknown> {
+    if (typeof endpoint === 'string') {
+      return callWithId(endpoint, parameters, callOptions)
+    }
+    return callWithObject(endpoint, parameters, callOptions)
   }
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -13,7 +13,7 @@ describe('apiFactory', () => {
       },
       response: {
         body: d => d.string('message'),
-        headers: d => d.string('content-type')
+        headers: d => d.const('content-type', 'application/json')
       }
     }),
     url: 'https://httpbin.org',
@@ -21,7 +21,7 @@ describe('apiFactory', () => {
   } as const
 
   it('should callable api returns', () => {
-    const api = apiFactory({}, async () => {
+    const api = apiFactory({}, {}, async () => {
       return {
         statusCode: 200,
         body: {
@@ -44,7 +44,7 @@ describe('apiFactory', () => {
         }
       }
 
-      const api = apiFactory(options, async () => {
+      const api = apiFactory(options, {}, async () => {
         return {
           statusCode: 200,
           body: {
@@ -92,7 +92,7 @@ describe('apiFactory', () => {
     })
 
     it('should callable successfully', async () => {
-      const api = apiFactory({}, async () => {
+      const api = apiFactory({}, {}, async () => {
         return {
           statusCode: 200,
           body: {
@@ -135,7 +135,7 @@ describe('apiFactory', () => {
         validateRequest: {
           throwError: true
         }
-      }, async () => {
+      }, {}, async () => {
         return {
           statusCode: 200,
           body: {
@@ -184,7 +184,188 @@ describe('apiFactory', () => {
         validateResponse: {
           throwError: true
         }
-      }, async () => {
+      }, {}, async () => {
+        return {
+          statusCode: 200,
+          body: {
+            message: 'hello!'
+          },
+          headers: {
+            'content-type': 'application/json+x'
+          }
+        }
+      })
+
+      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+      const call = async (options: CallOptions = {}) => {
+        return api(endpoint, {
+          query: {
+            message: 'hello!'
+          },
+          headers: {
+            authorization: '1234',
+            host: 'host'
+          }
+        }, options)
+      }
+
+      it('should validate successfully', async () => {
+        await expect(call()).rejects.toEqual({
+          property: 'instance.headers.content-type',
+          message: 'does not exactly match expected constant: application/json',
+          schema: { const: 'application/json' },
+          instance: 'application/json+x',
+          name: 'const',
+          argument: 'application/json',
+          stack: 'instance.headers.content-type does not exactly match expected constant: application/json'
+        })
+        expect(consoleOutput).toEqual([])
+      })
+
+      it('should validate successfully without throw', async () => {
+        await expect(call({ validateResponse: { throwError: false } })).resolves.toBeTruthy()
+        expect(consoleOutput).toEqual(['[api] response parameter validation failed.'])
+      })
+    })
+  })
+})
+
+describe('apiFactory with endpoints', () => {
+  const endpoints = {
+    'test.hello': {
+      ...endpointSchema({
+        request: {
+          query: d => d.string('message', {
+            pattern: '^hello'
+          }),
+          headers: d => d.string('authorization').string('host')
+        },
+        response: {
+          body: d => d.string('message'),
+          headers: d => d.const('content-type', 'application/json')
+        }
+      }),
+      url: 'https://httpbin.org',
+      method: 'get'
+    }
+  } as const
+
+  it('should callable api returns', () => {
+    const api = apiFactory({}, endpoints, async () => {
+      return {
+        statusCode: 200,
+        body: {
+          message: 'hello!'
+        },
+        headers: {
+          'content-type': 'application/json'
+        }
+      }
+    })
+
+    expect(api).toBeInstanceOf(Function)
+  })
+
+  describe('default options', () => {
+    describe('relax call type', () => {
+      const options = {
+        headers: {
+          authorization: '1234'
+        }
+      }
+
+      const api = apiFactory(options, endpoints, async () => {
+        return {
+          statusCode: 200,
+          body: {
+            message: 'hello!'
+          },
+          headers: {
+            'content-type': 'application/json'
+          }
+        }
+      })
+
+      it('should make optional', () => {
+        // `authorization` is not required because default options contains `headers.authorization`.
+        expect(() => api('test.hello', {
+          query: {
+            message: 'hello!'
+          },
+          headers: {
+            host: 'host'
+          }
+        })).not.toThrow()
+      })
+
+      // `authorization` is not required, but able to override.
+      expect(() => api('test.hello', {
+        query: {
+          message: 'hello!'
+        },
+        headers: {
+          host: 'host',
+          authorization: '1234'
+        }
+      })).not.toThrow()
+    })
+  })
+
+  describe('api', () => {
+    const consoleOutput: string[] = []
+    const mockedWarn = (output: string): void => {
+      consoleOutput.push(output)
+    }
+    beforeEach(() => {
+      console.warn = mockedWarn
+      consoleOutput.splice(0, consoleOutput.length)
+    })
+
+    it('should callable successfully', async () => {
+      const api = apiFactory({}, endpoints, async () => {
+        return {
+          statusCode: 200,
+          body: {
+            message: 'hello!'
+          },
+          headers: {
+            'content-type': 'application/json'
+          }
+        }
+      })
+
+      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+      const call = async () => {
+        return api('test.hello', {
+          query: {
+            message: 'hello!'
+          },
+          headers: {
+            authorization: '1234',
+            host: 'host'
+          }
+        })
+      }
+
+      await expect(call).not.toThrow()
+      await expect(call()).resolves.toStrictEqual({
+        body: {
+          message: 'hello!'
+        },
+        headers: {
+          'content-type': 'application/json'
+        },
+        statusCode: 200
+      })
+      expect(consoleOutput).toEqual([])
+    })
+
+    describe('request validation', () => {
+      const api = apiFactory({
+        validateRequest: {
+          throwError: true
+        }
+      }, endpoints, async () => {
         return {
           statusCode: 200,
           body: {
@@ -198,10 +379,56 @@ describe('apiFactory', () => {
 
       // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       const call = async (options: CallOptions = {}) => {
-        return api({
-          ...endpoint,
-          response: endpoint.response.omit('body').string('body')
-        }, {
+        return api('test.hello', {
+          query: {
+            message: 'good morning!'
+          },
+          headers: {
+            authorization: '1234',
+            host: 'host'
+          }
+        }, options)
+      }
+
+      it('should validate successfully', async () => {
+        await expect(call()).rejects.toEqual({
+          property: 'instance.queryStringParameters.message',
+          message: 'does not match pattern "^hello"',
+          schema: { type: 'string', pattern: '^hello' },
+          instance: 'good morning!',
+          name: 'pattern',
+          argument: '^hello',
+          stack: 'instance.queryStringParameters.message does not match pattern "^hello"'
+        })
+        expect(consoleOutput).toEqual([])
+      })
+
+      it('should validate successfully without throw', async () => {
+        await expect(call({ validateRequest: { throwError: false } })).resolves.toBeTruthy()
+        expect(consoleOutput).toEqual(['[api] request parameter validation failed.'])
+      })
+    })
+
+    describe('response validation', () => {
+      const api = apiFactory({
+        validateResponse: {
+          throwError: true
+        }
+      }, endpoints, async () => {
+        return {
+          statusCode: 200,
+          body: {
+            message: 'hello!'
+          },
+          headers: {
+            'content-type': 'application/json+x'
+          }
+        }
+      })
+
+      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+      const call = async (options: CallOptions = {}) => {
+        return api('test.hello', {
           query: {
             message: 'hello!'
           },
@@ -214,13 +441,13 @@ describe('apiFactory', () => {
 
       it('should validate successfully', async () => {
         await expect(call()).rejects.toEqual({
-          property: 'instance.body',
-          message: 'is not of a type(s) string',
-          schema: { type: 'string' },
-          instance: { message: 'hello!' },
-          name: 'type',
-          argument: ['string'],
-          stack: 'instance.body is not of a type(s) string'
+          property: 'instance.headers.content-type',
+          message: 'does not exactly match expected constant: application/json',
+          schema: { const: 'application/json' },
+          instance: 'application/json+x',
+          name: 'const',
+          argument: 'application/json',
+          stack: 'instance.headers.content-type does not exactly match expected constant: application/json'
         })
         expect(consoleOutput).toEqual([])
       })
